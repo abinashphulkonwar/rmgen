47e48f5512939927dbf7c3a9792c6927c4ab5195
abinash <abinashphulkonwar1@gmail.com>
2024-01-20 09:55:52 +0530

    add feature to output csv files

--- src/random/generated.go
+++ src/random/generated.go
@@ -5,18 +5,44 @@ import (
 	"github.com/brianvoe/gofakeit/v6"
 )
 
+const MAX_FILE_SIZE = 1024 * 1024 * 5
+
 func Generated() {
+	if file.OutputPointer == nil {
+		panic("Output pointer is nil")
+	}
 	if file.ConfigPointer == nil {
 		panic("Config pointer is nil")
 	}
-
-	for i := 0; i <= 10; i++ {
-		for _, v := range *file.ConfigPointer.Get() {
-			text := generated_handler(v.Type)
-			println(text)
+	buf := *new([]byte)
+	new_line := []byte("\n")
+	separator := []byte(",")
+	length := len((*file.ConfigPointer.Get()))
+	for index, v := range *file.ConfigPointer.Get() {
+		buf = append(buf, []byte(v.Type)...)
+		if index < length-1 {
+			buf = append(buf, separator...)
 		}
 	}
-
+	buf = append(buf, new_line...)
+	length_of_random_data := 10000
+	for i := 0; i < length_of_random_data; i++ {
+		for index, v := range *file.ConfigPointer.Get() {
+			text := generated_handler(v.Type)
+			buf = append(buf, []byte(text)...)
+			if index < length-1 {
+				buf = append(buf, separator...)
+			}
+		}
+		if i != length_of_random_data {
+			buf = append(buf, new_line...)
+		}
+		if len(buf) > MAX_FILE_SIZE {
+			file.OutputPointer.Write(&buf)
+			buf = *new([]byte)
+		}
+	}
+	file.OutputPointer.Write(&buf)
 }
 
 func generated_handler(Type string) string {
--- src/random/generated_test.go
+++ src/random/generated_test.go
@@ -1,6 +1,7 @@
 package random_test
 
 import (
+	"os"
 	"testing"
 
 	"github.com/abinashphulkonwar/go-random-data-generation/src/file"
@@ -8,8 +9,12 @@ import (
 )
 
 func TestRandomGeneration(t *testing.T) {
+	os.Setenv("mode", "test")
 	config := file.NewConfig("config.json")
+	output := file.NewOutput()
 	config.Init()
+	output.Init()
+	defer output.Close()
 	defer config.Close()
 	random.Generated()
 }
