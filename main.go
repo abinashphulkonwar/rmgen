12574c7ef2b1dba03a970ba153ddc1ac2dfcbcf8
abinash <abinashphulkonwar1@gmail.com>
2024-01-21 14:39:33 +0530

    add commands handler

--- main.go
+++ main.go
@@ -1,5 +1,39 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	commandshandler "github.com/abinashphulkonwar/go-random-data-generation/src/commands_handler"
+	"github.com/urfave/cli/v2"
+)
+
+func main() {
+	var file_path string
+	var count int
+	app := &cli.App{
+		Name:  "random data generation",
+		Usage: "make an explosive entrance",
+
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "file",
+				Usage:       "config file path",
+				Aliases:     []string{"f"},
+				Destination: &file_path,
+			},
+			&cli.IntFlag{
+				Name:        "count",
+				Usage:       "number of random data row",
+				Aliases:     []string{"c"},
+				Destination: &count,
+			},
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+
+	commandshandler.Handler(file_path, count)
 }
